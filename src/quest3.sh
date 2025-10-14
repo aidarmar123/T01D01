71abf1008dd0956dd74009fbf9d5f526ce1ffc2c
Liliia08 <125467004+Liliia08@users.noreply.github.com>
2025-10-14 08:05:55 +0300

    Задание 3 выполнено: файлы рассортированы, door management module запущен

---
+++ src/quest3.sh
@@ -0,0 +1,6 @@
+# Commands for Task 3 - Door Management
+cd door_management_files
+mkdir -p door_configuration door_map door_logs
+# Files were manually sorted
+cd ..
+bash door_management_files/ai_door_management_module.sh
