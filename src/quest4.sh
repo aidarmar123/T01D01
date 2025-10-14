353e4957229a9dbdad949954884c3053db9d9e99
Liliia08 <125467004+Liliia08@users.noreply.github.com>
2025-10-14 08:17:41 +0300

    Задание 4 выполнено: ai_door_control.sh полностью обезврежен

--- src/quest4.sh
+++ src/quest4.sh
@@ -1,1 +1,5 @@
 # Commands to stop ai_door_control.sh
+ps aux | grep ai_door_control.sh
+chmod -x door_management_files/ai_door_control.sh
+mv door_management_files/ai_door_control.sh door_management_files/ai_door_control.sh.disabled
+# Module disabled by removing execute permissions and renaming
