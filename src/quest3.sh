aa1bb0c80bf9d3082a6eda88e037164c12bb10d6
Liliia08 <125467004+Liliia08@users.noreply.github.com>
2025-10-14 15:40:47 +0300

    Задание 4 выполнено: остановлен ai_door_control.sh

--- src/quest3.sh
+++ src/quest3.sh
@@ -1,6 +1,5 @@
-# Commands for Task 3 - Door Management
-cd door_management_files
-mkdir -p door_configuration door_map door_logs
-# Files were manually sorted
-cd ..
-bash door_management_files/ai_door_management_module.sh
+# bin/bash
+# Commands for Task 3 - Door Management Module
+cat door_management_files/ai_door_management_module.sh
+cat ai_door_management_module.sh
+# Commands for Task 3 - Door Management Module
